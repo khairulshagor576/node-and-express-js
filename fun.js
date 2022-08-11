b7628dff395793c4f7d2e32e754f9ad58b9e570f
khairul islam <khairulsagor567@gmail.com>
2022-08-12 01:04:45 +0600

    practice with node file and node http module

---
+++ fun.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    fs.readFile('./index.html',(err,data)=>{
+        res.write(data);
+        res.end();
+    });
+});
+
+server.listen(4040,()=>console.log("Server is running at 4040 port"));
