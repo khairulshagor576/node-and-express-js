d25f914f4244600bbb37547109ce4fb5de144ee5
khairul islam <khairulsagor567@gmail.com>
2022-08-16 20:54:09 +0600

    route path practice here

--- express/app.js
+++ express/app.js
@@ -5,8 +5,19 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hello, Khairul islam");
+    res.json(
+        {
+            "name":"Khairul Islam",
+            "email":"khairulshagor@outlook.com",
+        }
+    );
 })
+
+app.get('/path',(req,res)=>{
+    console.dir(req.path)
+    //res.send(req.ip);
+})
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=>{
