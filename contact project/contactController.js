b243f418c068966f8becc278307387588e34f377
khairul islam <khairulsagor567@gmail.com>
2022-08-24 23:19:11 +0600

    get single id contact and update contact and delete contact

--- contact project/contactController.js
+++ contact project/contactController.js
@@ -11,6 +11,32 @@ exports.createContactNew = (req,res)=>{
   let contact = contacts.createContact({
     name,phone,email
   });
+  res.json(contact); 
+}
+
+exports.getContactsById=(req,res)=>
+{
+   let {id} = req.params;
+   id = parseInt(id);
+   let contact = contacts.getContactsById(id);
+   res.json(contact);
+}
+
+exports.updateContactById=(req,res)=>{
+  let {id} = req.params;
+  id= parseInt(id);
+  let {name,phone,email} = req.body;
+  let contact = contacts.updateContactById(id,{
+    name,phone,email
+  });
+  res.json(contact);
+}
+
+exports.deleteContactById=(req,res)=>
+{
+  let {id} = req.params;
+  id = parseInt(id);
+  let contact = contacts.deleteContactById(id);
   res.json(contact);
   
 }
--- contact project/contactRouter.js
+++ contact project/contactRouter.js
@@ -2,13 +2,16 @@
 const router = require('express').Router();
 const {
     getAllContacts,
-    createContactNew
+    createContactNew,
+    getContactsById,
+    updateContactById,
+    deleteContactById
 }=require('./contactController')
 
 router.get('/',getAllContacts);
 router.post('/',createContactNew);
-router.get('/:id');
-router.put('/:id');
-router.delete('/:id');
+router.get('/:id',getContactsById);
+router.put('/:id',updateContactById);
+router.delete('/:id',deleteContactById);
 
 module.exports= router;
